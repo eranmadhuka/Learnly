3669ba05e98f8a7223b99d32f9c198ea0fe61bc9
VikumBhashitha02 <vbhashitha02@gmail.com>
2025-05-11 11:00:03 +0530

    firebase
    firebase

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyByluQo17Gg_zYjNWNDpXDpe2QvfF9a74Q",
-  authDomain: "learnly-a1b21.firebaseapp.com",
-  projectId: "learnly-a1b21",
-  storageBucket: "learnly-a1b21.firebasestorage.app",
-  messagingSenderId: "518484503614",
-  appId: "1:518484503614:web:822492be76520b4390f56f",
-  measurementId: "G-YXTN8ZVG3V"
-};
+    apiKey: "AIzaSyByluQo17Gg_zYjNWNDpXDpe2QvfF9a74Q",
+    authDomain: "learnly-a1b21.firebaseapp.com",
+    projectId: "learnly-a1b21",
+    storageBucket: "learnly-a1b21.firebasestorage.app",
+    messagingSenderId: "518484503614",
+    appId: "1:518484503614:web:a481b8ebb60a15da90f56f",
+    measurementId: "G-KRCBE8TPBQ"
+  };
+  
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const storage = getStorage(app);
+
+
+export { storage };
